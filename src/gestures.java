6e8ff037ed0811c8e22ee518406975cc5ae731d7
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-23 23:53:37 -0300

    21. Ejecucion de secuencia de acciones tactiles

--- src/gestures.java
+++ src/gestures.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
 
@@ -6,8 +7,10 @@ import io.appium.java_client.TouchAction;
 
 
 import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import static java.time.Duration.ofSeconds;
 import static io.appium.java_client.touch.offset.ElementOption.element;
 
 public class gestures extends base{
@@ -15,13 +18,20 @@ public class gestures extends base{
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver = capabilities();
-		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
 		//Tap
 		TouchAction t = new TouchAction(driver);
 		WebElement expandList = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
 
 		t.tap(tapOptions().withElement(element(expandList))).perform();
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']").click();
+		WebElement pn = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+		
+		t.longPress(longPressOptions().withElement(element(pn)).withDuration(ofSeconds(2))).release().perform();
+		//Thread.sleep(2000)
+		System.out.println(driver.findElementById("android:id/title").isDisplayed());
 
 	}
 
