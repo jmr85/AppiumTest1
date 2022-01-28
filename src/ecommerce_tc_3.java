c62a6baca2a8b5dc1d05e58d77036028f115fd61
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-28 16:55:23 -0300

    36. Testcase-Scrolling con la lista de productos

--- src/ecommerce_tc_3.java
+++ src/ecommerce_tc_3.java
@@ -4,27 +4,26 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class ecommerce_tc_2 extends base{
+public class ecommerce_tc_3 extends base{
 
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
-		driver.hideKeyboard();
+		//driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+		//driver.hideKeyboard();
 		driver.findElement(By.xpath("//*[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
 		//if it doesn't work try this line
-		//driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"Argentina\").instance(0))"));    
+		//driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));    
 		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		System.out.println(driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size());
-		//Assert.assertEquals("Please enter your name", toastMessage);//Actual validation
-		//name attribute for toast messages will have content
+		driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
 	}
 
 }
