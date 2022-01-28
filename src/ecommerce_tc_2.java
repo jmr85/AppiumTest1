8f5cd33a0abe1a811eab03ba0b247cff4547cdc7
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-28 05:45:56 -0300

    34.35. Verificacion de mensajes Toast para validaciones de errores

--- src/ecommerce_tc_2.java
+++ src/ecommerce_tc_2.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.By;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class ecommerce_tc_1 extends base{
+public class ecommerce_tc_2 extends base{
 
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
-		driver.hideKeyboard();
+		//driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+		//driver.hideKeyboard();
 		driver.findElement(By.xpath("//*[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
 		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
@@ -21,6 +21,10 @@ public class ecommerce_tc_1 extends base{
 		//driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"Argentina\").instance(0))"));    
 		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		String toastMessage = driver.findElement(By.xpath("//android.widget.Toast[1]")).getAttribute("name");
+		System.out.println(toastMessage);
+		//Assert.assertEquals(“Please enter your name”, toastMessage);
+		//name attribute for toast messages will have content
 	}
 
 }
