d0dceaceb9b4865c425ffb75f72189d4e30c3116
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-29 13:18:03 -0300

    37. Testcase: seleccion del producto mediante scanning list basada en el texto

--- src/ecommerce_tc_3.java
+++ src/ecommerce_tc_3.java
@@ -2,7 +2,6 @@ import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
@@ -10,20 +9,33 @@ import io.appium.java_client.android.AndroidElement;
 
 public class ecommerce_tc_3 extends base{
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		//driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
-		//driver.hideKeyboard();
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+		driver.hideKeyboard();
 		driver.findElement(By.xpath("//*[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
-		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
 		//if it doesn't work try this line
 		//driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));    
 		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
+		driver.findElement(MobileBy.AndroidUIAutomator(
+				"new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
+		
+		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+		
+		for (int i = 0 ; i < count; i++) {
+			String text = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+			System.out.println(text);
+			if(text.equalsIgnoreCase("Jordan 6 Rings")) {
+				driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+				break;
+			}		
+		}
+		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 	}
 
 }
