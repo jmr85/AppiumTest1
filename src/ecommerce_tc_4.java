36e3b87572dbc13fce6b35a355c8b4b5d995b77c
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-30 04:54:39 -0300

    42. Optimizacion de codigo con funciones custom

--- src/ecommerce_tc_4.java
+++ src/ecommerce_tc_4.java
@@ -8,7 +8,7 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class ecommerce_tc_4 extends base{
+public class ecommerce_tc_4 extends base {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		// TODO Auto-generated method stub
@@ -18,30 +18,38 @@ public class ecommerce_tc_4 extends base{
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//*[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
-		//if it doesn't work try this line
-		//driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));    
+		driver.findElementByAndroidUIAutomator(
+				"new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+		// if it doesn't work try this line
+		// driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new
+		// UiSelector().scrollable(true).instance(0)).scrollIntoView(new
+		// UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
 		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		
+
 		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 		Thread.sleep(4000);
-		String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0).getText();//$120.0 
-		amount1 = amount1.substring(1);//120.0
-		double amount1value = Double.parseDouble(amount1);
-		String amount2 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(1).getText();
-		amount2 = amount2.substring(1);
-		//160.97
-		double amount2value = Double.parseDouble(amount2);
-		double sumOfProducts = amount1value + amount2value;
-		System.out.println("sum of products: " + sumOfProducts);
+		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
+		double sum = 0;
+		for (int i = 0; i < count; i++) {
+			String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i)
+					.getText();
+			double amount = getAmount(amount1);
+			sum += amount;// 280.97
+		}
+		System.out.println("sum of products: " + sum);
 		String total = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
 		total = total.substring(1);
 		double totalvalue = Double.parseDouble(total);
 		System.out.println("Total value of products: " + totalvalue);
-		Assert.assertEquals(sumOfProducts, totalvalue);
+		Assert.assertEquals(sum, totalvalue);
+	}
+
+	public static double getAmount(String value) {
+		value = value.substring(1);
+		return Double.parseDouble(value);
 	}
 
 }
