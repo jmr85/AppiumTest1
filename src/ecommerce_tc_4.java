146edc99886f98f7cce18c8ab981d168c22284e3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-30 04:08:46 -0300

    41. TestCase - Validacion de la funcionalidad de la cantidad total generada

--- src/ecommerce_tc_4.java
+++ src/ecommerce_tc_4.java
@@ -27,6 +27,21 @@ public class ecommerce_tc_4 extends base{
 		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+		Thread.sleep(4000);
+		String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0).getText();//$120.0 
+		amount1 = amount1.substring(1);//120.0
+		double amount1value = Double.parseDouble(amount1);
+		String amount2 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(1).getText();
+		amount2 = amount2.substring(1);
+		//160.97
+		double amount2value = Double.parseDouble(amount2);
+		double sumOfProducts = amount1value + amount2value;
+		System.out.println("sum of products: " + sumOfProducts);
+		String total = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
+		total = total.substring(1);
+		double totalvalue = Double.parseDouble(total);
+		System.out.println("Total value of products: " + totalvalue);
+		Assert.assertEquals(sumOfProducts, totalvalue);
 	}
 
 }
