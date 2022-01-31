d2f62d3bd35cb126171d390cee68a3b697532e04
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-01-30 21:48:16 -0300

    Hybrid App Automation - 44. Testcase - Validacion Tap and Long Press

--- src/ecommerce_tc_4.java
+++ src/ecommerce_tc_4.java
@@ -1,10 +1,17 @@
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -45,6 +52,15 @@ public class ecommerce_tc_4 extends base {
 		double totalvalue = Double.parseDouble(total);
 		System.out.println("Total value of products: " + totalvalue);
 		Assert.assertEquals(sum, totalvalue);
+		
+		//Mobile gestures
+		WebElement checkbox = driver.findElement(By.className("android.widget.CheckBox"));
+		TouchAction t = new TouchAction(driver);
+		t.tap(tapOptions().withElement(element(checkbox))).perform();
+		WebElement tc = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
+		t.longPress(longPressOptions().withElement(element(tc)).withDuration(ofSeconds(2))).release().perform();
+		driver.findElement(By.id("android:id/button1")).click();//Close button - Terms Of conditions popup
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
 	}
 
 	public static double getAmount(String value) {
