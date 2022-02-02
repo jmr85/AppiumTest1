dcaa4c1064af4d8c9dafa2f8939939ef53c81455
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-02 09:17:27 -0300

    Hybrid App Automation - 47.2 Capabilities para ejecutar pruebas Appium en el browser mobile

--- src/browse.java
+++ src/browse.java
@@ -10,14 +10,14 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class browse {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver;
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 10 API 29");//Emulator
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		driver = new AndroidDriver<>(cap);
+		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 	}
 
 }
