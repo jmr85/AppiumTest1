bd5cb97f7ec3c8567f9db40f3f3884a9db407b79
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-02 12:23:10 -0300

    Mobile Chrome Browser - 49. Scroll y Asercion en la prueba del browser con Appium

--- src/browse.java
+++ src/browse.java
@@ -1,9 +1,12 @@
 import java.net.MalformedURLException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+
 
 public class browse extends baseChrome {
 
@@ -13,7 +16,10 @@ public class browse extends baseChrome {
 		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
 		driver.findElement(By.cssSelector(".navbar-toggler")).click();
 		driver.findElement(By.cssSelector("a[href*='products']")).click();
-
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0, 1000)", "");
+		String text =driver.findElement(By.xpath("(//li[@class='list-group-item'])[3]/div/div/a")).getText();
+		Assert.assertEquals(text, "Devops");
 	}
 
 }
